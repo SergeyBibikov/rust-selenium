95322d9e7fd73406f132acb9980a38b772069cac
Sergey Bibikov <sergey.bibikov.92@mail.ru>
2020-10-27 16:48:31 +0300

    test for windows

--- src/tests.rs
+++ src/tests.rs
@@ -1,13 +1,33 @@
 #[cfg(test)]
 mod tests {
     use super::super::reqs::*;
-
+    #[cfg(target_os = "linux")]
     #[test]
-    fn session_id() {
+    fn session_id_linux() {
         let temp = serde_json::json!({
             "capabilities": {
                 "alwaysMatch": {
                     "platformName": "linux"
+                },
+                "firstMatch": [
+                    {"browserName": "chrome"}
+                ]
+            }
+        });
+        let body = serde_json::to_string(&temp).unwrap();
+        //println!("{}", body);
+        let length = body.len();
+        let headers = vec![format!("Content-Length:{}", length + 2)];
+        let response = send_request(Method::POST, "wd/hub/session", headers, &body).unwrap();
+        assert!(response.contains("sessionId"));
+    }
+    #[cfg(target_os = "windows")]
+    #[test]
+    fn session_id_windows() {
+        let temp = serde_json::json!({
+            "capabilities": {
+                "alwaysMatch": {
+                    "platformName": "windows"
                 },
                 "firstMatch": [
                     {"browserName": "chrome"}
